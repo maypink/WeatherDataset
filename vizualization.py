1313137d29b8cc5a5d39815dfc475342b8ea0388
Pinchuk Maya <57305249+maypink@users.noreply.github.com>
2021-03-02 01:42:59 +0300

    Update vizualization.py

--- vizualization.py
+++ vizualization.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 
-def vizualization_plt(test_loss_history, test_accuracy_history):
+def vizualization_plt(test_loss_history, test_accuracy_history, train_loss_history, train_accuracy_history):
     plt.plot(test_loss_history)
     plt.plot(test_accuracy_history)
+    plt.show()
+
+    plt.plot(train_loss_history)
+    plt.plot(train_accuracy_history)
     plt.show()
