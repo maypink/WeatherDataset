ad61ab4a643681bcf4068299437099fa9c4600ee
Pinchuk Maya <57305249+maypink@users.noreply.github.com>
2021-03-03 23:26:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from vizualization import vizualization_plt
 
 data_preparation.reprod_init()
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
 x_train, x_test, y_train, y_test = data_preparation.get_data()
 
 composed = torchvision.transforms.Compose([data_preparation.resize, data_preparation.to_tensor])
@@ -20,6 +22,6 @@ test_loader = torch.utils.data.DataLoader(test, batch_size=224, shuffle=True)
 
 weather_net = WeatherNet()
 
-test_loss_history, test_accuracy_history, train_loss_history, train_accuracy_history = training(weather_net, train_loader, test_loader)
+test_loss_history, test_accuracy_history, train_loss_history, train_accuracy_history = training(weather_net, train_loader, test_loader, device)
 
 vizualization_plt(test_loss_history, test_accuracy_history, train_loss_history, train_accuracy_history)
