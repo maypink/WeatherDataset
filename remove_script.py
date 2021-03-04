4928281930977c37e80c48240c2261b772d7d3ec
Pinchuk Maya <57305249+maypink@users.noreply.github.com>
2021-03-04 21:20:21 +0300

    Update remove_script.py

--- remove_script.py
+++ remove_script.py
@@ -5,15 +5,14 @@ print('Removing inappropriate images...')
 print('Images that have been removed:')
 for (dirpath, dirname, filenames) in os.walk('dataset2'):
     for filename in filenames:
-        if filename.endswith('jpg'):
-            label = ''
-            for i in filename:
-                if i.isalpha():
-                    label+=i
-                else:
-                    break
-            filepath = os.path.join(dirpath, filename)
-            image = imread(filepath)
-            if len(image.shape)!=3 or image.shape[2]!=3 or label not in ['sunrise', 'rain', 'cloudy', 'shine']:
-                print(filepath)
-                os.remove(filepath)
+        label = ''
+        for i in filename:
+            if i.isalpha():
+                label+=i
+            else:
+                break
+        filepath = os.path.join(dirpath, filename)
+        image = imread(filepath)
+        if not filename.endswith('.jpg') or len(image.shape)!=3 or image.shape[2]!=3 or label not in ['sunrise', 'rain', 'cloudy', 'shine']:
+            print(filepath)
+            os.remove(filepath)
